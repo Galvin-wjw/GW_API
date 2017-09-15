5459eb6535f1ec28823425dbc067a71ae2b6fd59
Galvin_wjw <137606834@qq.com>
2017-09-15 16:53:06 +0800

    k line
    k line

--- his_data_K_line.py
+++ his_data_K_line.py
@@ -1,26 +1,27 @@
 # coding:utf-8
 import requests
 import json
+import time
+import numpy
 import matplotlib.finance as mpf
 import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY,YEARLY
-import matplotlib
+import matplotlib.ticker as mticker
 
 def send_requset():
     url = "https://trade.api.lwork.com/v1/chart"
     headers = {
         "Host":"trade.api.lwork.com",
         "content-type":"application/json",
-        "x-api-tenantid": "T90000",
+        "x-api-tenantid": "T900002",
         "x-api-token": "BpUPBvRl4e3wiIOU",
         "x-api-serverid":'73',
     }
 
     params = {
-                 "vendor": "mt4",
+        "vendor": "mt4",
         "period": "d1",
         "from":"2017-08-01T00:00:01Z",
-        "to":"2017-08-19T00:00:01Z",
+        "to":"2017-09-01T00:00:01Z",
         "symbol":"EURUSD"
     }
 
@@ -29,57 +30,43 @@ def send_requset():
         headers = headers,
         params = params
     )
+    return response
 
-    # print(response.url)
-    # print(response.headers)
-    #
-    # print('Response HTTP Status Code: {status_code}'.format(
-    #     status_code=response.status_code))
-    # print('Response HTTP Response Body: {content}'.format(
-    #     content=response.content))
-
-    #print(response.text)
-   # data_list = {}
+if __name__ == '__main__':
+    response = send_requset()
+    #处理从接口获取的数据
+    trade_time = []
     open = []
     close = []
     high = []
     low = []
-
     data_list = json.loads(response.text)
     data = data_list['data']
-    #print(data)
+    format = '%m-%d-%H:%M'
     for i in range(len(data)):
+        trade_time.append(data[i]['timestamp'])
         close.append(data[i]['close'])
         open.append(data[i]['open'])
         high.append(data[i]['high'])
         low.append(data[i]['low'])
+    for i in range(len(trade_time)):
+        trade_time[i] = time.localtime(trade_time[i])#经过localtime转换后变成time.struct_time
+        trade_time[i] = time.strftime(format,trade_time[i])
+    #print(trade_time)
+    fig, ax1 = plt.subplots()
+    fig.subplots_adjust(bottom=0.2)
+    plt.suptitle("EURUSD-D1-K-line")
 
-
-    ax1 = plt.subplot2grid((6, 4), (1, 0), rowspan=4, colspan=4, axisbg='w')
-    plt.suptitle("subplot2grid")
-
-    monthdays = matplotlib.dates.MonthLocator()
-    mondays = WeekdayLocator(MONDAY)
-    alldays = DayLocator()
-
-    #设定日期的主次刻度
-    ax1.xaxis.set_major_locator(mondays)
-    ax1.xaxis.set_minor_locator(alldays)
-
-
-    mondayFormatter = DateFormatter('%Y-%m-%d')  # 如：2-29-2015
-    ax1.xaxis.set_major_formatter(mondayFormatter)
-
-    ax1.xaxis_date()
-    mpf.candlestick2_ohlc(ax1, open, high, low,
-                          close, width=.75, colorup='r', colordown='g',alpha=0.75)
-
+    #设置横坐标
+    x = numpy.arange(len(trade_time))
+    plt.xticks(x,trade_time)
+    ax1.xaxis.set_major_locator(mticker.MaxNLocator(9))
     for label in ax1.xaxis.get_ticklabels():
-        label.set_rotation(90)
+        label.set_rotation(60)
 
-    #plt.grid(True)
+    #绘制K线图
+    mpf.candlestick2_ohlc(ax1, open, high, low,
+                          close, width=.75, colorup='r', colordown='g',alpha=1)
+
+    plt.grid(True)
     plt.show()
-
-
-if __name__ == '__main__':
-    send_requset()
